b7644c4873bb81a6dc8ec9c5a6fef8b8c1ca6e53
Kateryna Polikarpova <polikarpovakateryna@mail.com>
2020-01-04 20:36:49 -0500

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -21,10 +17,10 @@ namespace API
             {
                 var services = scope.ServiceProvider;
                 try 
-                
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
